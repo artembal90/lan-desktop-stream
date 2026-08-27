e5c00caf509887c7ceeeab03c13c5a99cac7ea81
artembal90 <artembal90@gmail.com>
2026-08-28 01:48:03 +0300

    Add Electron preload bridge

---
+++ src/preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('api', {
+  getSources:()=>ipcRenderer.invoke('sources'),
+  loadConfig:()=>ipcRenderer.invoke('config-load'),
+  saveConfig:c=>ipcRenderer.invoke('config-save',c),
+  selectSource:id=>ipcRenderer.invoke('select-source',id),
+  start:c=>ipcRenderer.invoke('start-server',c),
+  stop:()=>ipcRenderer.invoke('stop-server'),
+  onEvent:cb=>{const fn=(_e,m)=>cb(m);ipcRenderer.on('server-event',fn);return()=>ipcRenderer.removeListener('server-event',fn)}
+});
