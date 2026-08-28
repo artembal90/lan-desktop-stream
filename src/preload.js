86cce376ad475cf0a954b9ed97fb18d7cb157bd6
artembal90 <artembal90@gmail.com>
2026-08-28 04:10:24 +0300

    feat: expose receiver disconnect command

--- src/preload.js
+++ src/preload.js
@@ -6,5 +6,6 @@ contextBridge.exposeInMainWorld('api', {
   selectSource:id=>ipcRenderer.invoke('select-source',id),
   start:c=>ipcRenderer.invoke('start-server',c),
   stop:()=>ipcRenderer.invoke('stop-server'),
+  disconnectReceiver:id=>ipcRenderer.invoke('disconnect-receiver',id),
   onEvent:cb=>{const fn=(_e,m)=>cb(m);ipcRenderer.on('server-event',fn);return()=>ipcRenderer.removeListener('server-event',fn)}
 });
